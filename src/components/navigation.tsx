1a0af43ce9511abd367589510b85a10e73e3ddac
Rafael Zoucas <rafaelpzoucas@gmail.com>
2024-04-11 14:39:42 -0300

    Update navigation.tsx

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -12,77 +12,37 @@ export function Navigation() {
   const iconsClassNames = 'w-5 h-5'
 
   const isRootPathname =
-    pathname === '/orders' ||
-    pathname === '/store' ||
-    pathname === '/dashboard' ||
-    pathname === '/customers' ||
-    pathname === '/reports'
+    pathname === '/admin/orders' ||
+    pathname === '/admin/store' ||
+    pathname === '/admin/dashboard' ||
+    pathname === '/admin/customers' ||
+    pathname === '/admin/reports'
 
   const links = [
     {
-      href: '/dashboard',
+      href: '/admin/dashboard',
       name: 'Home',
-      icon: (
-        <Home
-          className={cn(
-            iconsClassNames,
-            'opacity-50',
-            pathname.startsWith('/dashboard') && 'opacity-100',
-          )}
-        />
-      ),
+      icon: Home,
     },
     {
-      href: '/orders',
+      href: '/admin/orders',
       name: 'Pedidos',
-      icon: (
-        <Layers
-          className={cn(
-            iconsClassNames,
-            'opacity-50',
-            pathname.startsWith('/orders') && 'opacity-100',
-          )}
-        />
-      ),
+      icon: Layers,
     },
     {
-      href: '/store',
+      href: '/admin/store',
       name: 'Minha loja',
-      icon: (
-        <Store
-          className={cn(
-            iconsClassNames,
-            'opacity-50',
-            pathname.startsWith('/store') && 'opacity-100',
-          )}
-        />
-      ),
+      icon: Store,
     },
     {
-      href: '/customers',
+      href: '/admin/customers',
       name: 'Clientes',
-      icon: (
-        <Users
-          className={cn(
-            iconsClassNames,
-            'opacity-50',
-            pathname.startsWith('/customers') && 'opacity-100',
-          )}
-        />
-      ),
+      icon: Users,
     },
     {
-      href: '/reports',
+      href: '/admin/reports',
       name: 'Relatórios',
-      icon: (
-        <BarChart2
-          className={cn(
-            iconsClassNames,
-            'opacity-50',
-            pathname.startsWith('/reports') && 'opacity-100',
-          )}
-        />
-      ),
+      icon: BarChart2,
     },
   ]
 
@@ -93,10 +53,16 @@ export function Navigation() {
         !isRootPathname && 'translate-y-16 duration-200',
       )}
     >
-      <Card className="flex flex-row lg:flex-col items-center justify-around gap-4 w-fit p-1 px-2 bg-secondary">
+      <Card className="flex flex-row lg:flex-col items-center justify-around gap-4 w-fit p-1 px-2 bg-background">
         {links.map((link) => (
           <Link href={link.href} key={link.href} className="relative p-2">
-            {link.icon}
+            <link.icon
+              className={cn(
+                iconsClassNames,
+                'opacity-50',
+                pathname.startsWith(link.href) && 'opacity-100',
+              )}
+            />
             {pathname.startsWith(link.href) && (
               <span className="absolute left-1/2 -translate-x-1/2 w-3 h-[3px] rounded-lg mt-1 bg-primary"></span>
             )}
