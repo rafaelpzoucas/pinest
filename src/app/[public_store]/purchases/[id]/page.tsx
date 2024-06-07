f5aa88d01e7b7540540c838e3bbc727cb42f8a68
Rafael Zoucas <rafaelpzoucas@gmail.com>
2024-06-07 19:36:28 -0300

    fix: purchase page

--- src/app/[public_store]/purchases/[id]/page.tsx
+++ src/app/[public_store]/purchases/[id]/page.tsx
@@ -18,51 +18,53 @@ export default async function PurchasePage({
     <section className="p-4">
       <Header title="Detalhes do pedido" />
 
-      <div className="flex flex-col gap-2">
-        <Status purchase={purchase} />
+      {purchase && (
+        <div className="flex flex-col gap-2">
+          <Status purchase={purchase} />
 
-        <Card className="p-4">
-          <p>
-            {purchase && formatDate(purchase?.created_at, 'dd/MM HH:mm:ss')}
-          </p>
-          <p className="flex flex-row items-center justify-between">
-            <span>Produtos(3)</span>
-            <span>R$ 100,00</span>
-          </p>
-          <p className="flex flex-row items-center justify-between">
-            <span>Desconto</span>
-            <span>R$ 0,00</span>
-          </p>
-          <p className="flex flex-row items-center justify-between">
-            <span>Frete</span>
-            <span>Grátis</span>
-          </p>
-          <p className="flex flex-row items-center justify-between">
-            <span>Total</span>
-            <span>R$ 100,00</span>
-          </p>
-        </Card>
+          <Card className="p-4">
+            <p>
+              {purchase && formatDate(purchase?.created_at, 'dd/MM HH:mm:ss')}
+            </p>
+            <p className="flex flex-row items-center justify-between">
+              <span>Produtos(3)</span>
+              <span>R$ 100,00</span>
+            </p>
+            <p className="flex flex-row items-center justify-between">
+              <span>Desconto</span>
+              <span>R$ 0,00</span>
+            </p>
+            <p className="flex flex-row items-center justify-between">
+              <span>Frete</span>
+              <span>Grátis</span>
+            </p>
+            <p className="flex flex-row items-center justify-between">
+              <span>Total</span>
+              <span>R$ 100,00</span>
+            </p>
+          </Card>
 
-        <Card className="p-4">
-          <p>R$ 100,00</p>
-          <span>PIX</span>
-        </Card>
+          <Card className="p-4">
+            <p>R$ 100,00</p>
+            <span>PIX</span>
+          </Card>
 
-        <Card className="p-4">
-          <p>Rua Santa Cruz, 801</p>
-          <span>Assis/SP</span>
-        </Card>
+          <Card className="p-4">
+            <p>Rua Santa Cruz, 801</p>
+            <span>Assis/SP</span>
+          </Card>
 
-        <Card className="p-4">
-          <ul>
-            <li>
-              <div>
-                <ProductCard data={purchase?.purchase_items[0].products} />
-              </div>
-            </li>
-          </ul>
-        </Card>
-      </div>
+          <Card className="p-4">
+            <ul>
+              <li>
+                <div>
+                  <ProductCard data={purchase?.purchase_items[0].products} />
+                </div>
+              </li>
+            </ul>
+          </Card>
+        </div>
+      )}
     </section>
   )
 }
