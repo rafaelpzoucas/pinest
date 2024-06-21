798743b7f099d64820ed39f69bf7f871de76f6cf
Rafael Zoucas <rafaelpzoucas@gmail.com>
2024-06-21 15:15:19 -0300

    refactor: customer can only see his own purchases

--- src/app/[public_store]/purchases/actions.ts
+++ src/app/[public_store]/purchases/actions.ts
@@ -7,19 +7,30 @@ export async function readPurchases(): Promise<{
 }> {
   const supabase = createClient()
 
+  const { data: session } = await supabase.auth.getUser()
+
+  const { data: customer, error: customerError } = await supabase
+    .from('customers')
+    .select('id')
+    .eq('user_id', session.user?.id)
+    .single()
+
+  console.error(customerError)
+
   const { data: purchases, error } = await supabase
     .from('purchases')
     .select(
       `
-    *,
-    purchase_items (
-      *,
-      products (
-        *
-      )
+        *,
+        purchase_items (
+          *,
+          products (
+            *
+          )
+        )
+      `,
     )
-  `,
-    )
+    .eq('customer_id', customer?.id)
     .order('created_at', { ascending: false })
 
   return { purchases, error }
