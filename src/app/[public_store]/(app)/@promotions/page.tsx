072ecac5d2fb0154024b4962ea15d84002330f27
Rafael Zoucas <rafaelpzoucas@gmail.com>
2024-06-22 10:32:49 -0300

    feat: add dynamic render to `promotions` section

--- src/app/[public_store]/(app)/@promotions/page.tsx
+++ src/app/[public_store]/(app)/@promotions/page.tsx
@@ -4,8 +4,21 @@ import {
   CarouselContent,
   CarouselItem,
 } from '@/components/ui/carousel'
+import { createClient } from '@/lib/supabase/server'
 
-export default function PromotionsPage() {
+export default async function PromotionsPage() {
+  const supabase = createClient()
+
+  const { data, error } = await supabase.from('promotions').select('*')
+
+  if (error) {
+    console.error(error)
+  }
+
+  if (!data || data.length === 0) {
+    return null
+  }
+
   return (
     <section className="space-y-4 p-4">
       <h1 className="text-lg font-bold uppercase">Promoções</h1>
