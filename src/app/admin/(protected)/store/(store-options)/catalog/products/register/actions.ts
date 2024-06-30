ae75900ac0d6f617784d417b8ad17846bc408122
Rafael Zoucas <rafaelpzoucas@gmail.com>
2024-06-30 16:12:08 -0300

    fix: product creation

--- src/app/admin/(protected)/store/(store-options)/catalog/products/register/actions.ts
+++ src/app/admin/(protected)/store/(store-options)/catalog/products/register/actions.ts
@@ -1,17 +1,46 @@
 'use server'
 
 import { createClient } from '@/lib/supabase/server'
+import { StoreType } from '@/models/store'
 import { revalidatePath } from 'next/cache'
 import { z } from 'zod'
 import { newProductFormSchema } from './form'
+
+export async function readStoreByUserId(): Promise<{
+  store: StoreType | null
+  storeError: any | null
+}> {
+  const supabase = createClient()
+
+  const { data: session, error: sessionError } = await supabase.auth.getUser()
+
+  if (sessionError) {
+    console.error(sessionError)
+  }
+
+  const { data: store, error: storeError } = await supabase
+    .from('stores')
+    .select('*')
+    .eq('user_id', session.user?.id)
+    .single()
+
+  return { store, storeError }
+}
 
 export async function createProduct(
   values: z.infer<typeof newProductFormSchema>,
 ) {
   const supabase = createClient()
+
+  const { store, storeError } = await readStoreByUserId()
+
+  if (storeError) {
+    console.error(storeError)
+  }
+
   const { data, error } = await supabase
     .from('products')
-    .insert([values])
+    .insert({ ...values, store_id: store?.id })
     .select()
 
   revalidatePath('/catalog')
