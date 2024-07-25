369c52fccac9948fb98ceffa1c60fe44d3f070ee
Rafael Zoucas <rafaelpzoucas@gmail.com>
2024-07-25 13:17:45 -0300

    fix: adjust type definition for PublicStoreLayout to resolve deployment error

--- src/app/[public_store]/layout.tsx
+++ src/app/[public_store]/layout.tsx
@@ -1,30 +1,37 @@
+import { CartProductType } from '@/models/cart'
 import { Metadata } from 'next'
+import { getStoreByStoreURL } from './actions'
+import { getCart } from './cart/actions'
+import { MobileNavigation } from './mobile-navigation'
+import NotFound from './not-found'
 
 export const metadata: Metadata = {
   title: 'Loja | Pinest',
   description: 'Loja virtual criada com a Pinest',
 }
 
+type PublicStoreLayoutProps = {
+  header: React.ReactNode
+  children: React.ReactNode
+  params: { public_store: string }
+}
+
 export default async function PublicStoreLayout({
   header,
   children,
   params,
-}: Readonly<{
-  header: React.ReactNode
-  children: React.ReactNode
-  params: { public_store: string }
-}>) {
-  // const { store, storeError } = await getStoreByStoreURL(params.public_store)
+}: PublicStoreLayoutProps) {
+  const { store, storeError } = await getStoreByStoreURL(params.public_store)
 
-  // if (storeError) {
-  //   console.log(storeError)
-  // }
+  if (storeError) {
+    console.log(storeError)
+  }
 
-  // if (!store) {
-  //   return <NotFound />
-  // }
+  if (!store) {
+    return <NotFound />
+  }
 
-  // const bagItems: CartProductType[] = await getCart(params.public_store)
+  const bagItems: CartProductType[] = await getCart(params.public_store)
 
   return (
     <div className="flex lg:flex-row items-center justify-center p-4 pb-20">
@@ -33,7 +40,7 @@ export default async function PublicStoreLayout({
 
         {children}
 
-        {/* <MobileNavigation bagItems={bagItems} /> */}
+        <MobileNavigation bagItems={bagItems} />
       </div>
     </div>
   )
