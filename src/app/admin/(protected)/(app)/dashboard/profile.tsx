242409e15f353b9c7faa144dbc5dc2c2d05c204f
Rafael Zoucas <rafaelpzoucas@gmail.com>
2024-11-04 13:41:20 -0300

    feat: created new elements on dashboard

--- src/app/admin/(protected)/(app)/dashboard/profile.tsx
+++ src/app/admin/(protected)/(app)/dashboard/profile.tsx
@@ -1,26 +1,34 @@
 import { buttonVariants } from '@/components/ui/button'
-import { Card } from '@/components/ui/card'
-import { ExternalLink } from 'lucide-react'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { cn, formatAddress } from '@/lib/utils'
+import { StoreType } from '@/models/store'
+import { ExternalLink, MapPin, Phone } from 'lucide-react'
 import Link from 'next/link'
-import { readStore } from './actions'
 
-export async function ProfileCard() {
-  const { store, storeError } = await readStore()
-
-  if (storeError) {
-    console.error(storeError)
-  }
-
+export async function ProfileCard({ store }: { store: StoreType }) {
   return (
-    <Card className="flex flex-col gap-6 p-4 max-w-sm">
-      <Link
-        href={`/${store?.store_url}`}
-        target="_blank"
-        className={buttonVariants()}
-      >
-        Ver minha loja
-        <ExternalLink className="w-4 h-4 ml-2" />
-      </Link>
+    <Card className="max-w-sm">
+      <CardHeader>
+        <CardTitle>{store?.name}</CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <span className="flex flex-row gap-2 text-sm text-muted-foreground">
+          <MapPin className="w-4 h-4" />
+          {formatAddress(store?.addresses[0])}
+        </span>
+        <span className="flex flex-row gap-2 text-sm text-muted-foreground">
+          <Phone className="w-4 h-4" />
+          {store?.phone}
+        </span>
+        <Link
+          href={`/${store?.store_url}`}
+          target="_blank"
+          className={cn(buttonVariants(), 'w-full')}
+        >
+          Ver minha loja
+          <ExternalLink className="w-4 h-4 ml-2" />
+        </Link>
+      </CardContent>
     </Card>
   )
 }
