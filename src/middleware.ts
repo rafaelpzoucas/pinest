031b86b51e81de53751b55ddcfbff0e9161e9f7e
Rafael Zoucas <rafaelpzoucas@gmail.com>
2025-04-02 10:30:56 -0300

    refactor: undo configs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,20 @@
 import { updateSession } from '@/lib/supabase/middleware'
-import { NextResponse, type NextRequest } from 'next/server'
+import { type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  // Atualiza a sessão primeiro
-  const sessionResponse = await updateSession(request)
-  if (sessionResponse) return sessionResponse
-
-  // Obtém o domínio acessado pelo usuário
-  const host = request.headers.get('host') || ''
-
-  // Mapeamento de domínios para seus respectivos caminhos
-  const domainMappings: Record<string, string> = {
-    'sandubadaleyla.com.br': '/sanduba-da-leyla',
-    'outrocliente.com.br': '/outro-cliente',
-    // Adicione mais domínios conforme necessário
-  }
-
-  // Se o domínio existir no mapeamento, faz o rewrite para o caminho correto
-  if (domainMappings[host]) {
-    const url = request.nextUrl.clone()
-    url.pathname = domainMappings[host] + url.pathname // Mantém o subcaminho
-    return NextResponse.rewrite(url)
-  }
-
-  return NextResponse.next()
+  return await updateSession(request)
 }
 
 export const config = {
   matcher: [
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
+     * Feel free to modify this pattern to include more paths.
+     */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
