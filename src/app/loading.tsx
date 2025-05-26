953fda7f5fde0bee27b4ccb45580b36708a3cdb1
Rafael Zoucas <rafaelpzoucas@gmail.com>
2025-05-26 14:21:52 -0300

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,16 @@
+import Image from 'next/image'
+
 export default function LoadingStore() {
-  return <div>Carregando...</div>
+  return (
+    <div className="w-screen h-dvh flex flex-col items-center justify-center">
+      <div className="relative w-16 h-16 animate-bounce">
+        <Image
+          src="/icon-dark.svg"
+          fill
+          alt="Carregando..."
+          className="object-fill"
+        />
+      </div>
+    </div>
+  )
 }
