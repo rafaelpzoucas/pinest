bfbb8b24bf547c4504a824b0b2d538bce40e2f84
Rafael Zoucas <rafaelpzoucas@gmail.com>
2025-05-26 14:36:44 -0300

    test: subdomains configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,35 @@
 import { updateSession } from '@/lib/supabase/middleware'
-import { type NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
+
+const IGNORED_HOSTS = ['localhost:3000', 'pinest.com.br']
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  const response = await updateSession(request)
+
+  const { nextUrl, headers } = request
+  const hostname = headers.get('host') || ''
+  const isIgnored = IGNORED_HOSTS.includes(hostname)
+
+  if (!isIgnored) {
+    const subdomain = hostname.split('.')[0]
+
+    // Protege contra www ou outros subdomínios inválidos
+    if (subdomain && subdomain !== 'www' && subdomain !== 'pinest') {
+      const pathname = nextUrl.pathname === '/' ? '' : nextUrl.pathname
+
+      const url = request.nextUrl.clone()
+      url.pathname = `/${subdomain}${pathname}`
+
+      return NextResponse.rewrite(url)
+    }
+  }
+
+  return response
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
+    // ignora arquivos estáticos
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
