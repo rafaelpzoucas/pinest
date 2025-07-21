13633ac69a386ad9f375b150f7e34285e5dbc230
Rafael Zoucas <rafaelpzoucas@gmail.com>
2025-07-21 14:30:25 -0300

    fix(store-status): move store status update to useEffect and fix hook usage

--- src/app/store-status.tsx
+++ src/app/store-status.tsx
@@ -4,40 +4,36 @@ import { storeShouldBeOpen } from '@/actions/client/app/public_store/status'
 import { usePublicStore } from '@/stores/public-store'
 import { useMutation } from '@tanstack/react-query'
 import { format, isAfter, isBefore, parse } from 'date-fns'
+import { useEffect } from 'react'
 
 export function StoreStatus() {
   const { store } = usePublicStore()
 
-  if (!store || store?.is_open_override) {
-    return null
-  }
+  const { mutateAsync: updateStoreStatus } = useMutation({
+    mutationKey: ['store-status'],
+    mutationFn: (shouldBeOpen: boolean) =>
+      storeShouldBeOpen(shouldBeOpen, store?.id),
+    onError: () => console.error('Erro ao atualizar status da loja'),
+  })
 
-  const now = new Date()
-  const today = format(now, 'EEEE').toLowerCase() // Pega o dia da semana em inglês
+  useEffect(() => {
+    if (!store || store?.is_open_override) return
 
-  // Filtrar o horário de hoje
-  const todayHours = store?.store_hours.find(
-    (hour: any) => hour.day_of_week === today,
-  )
+    const now = new Date()
+    const today = format(now, 'EEEE').toLowerCase()
+    const todayHours = store?.store_hours.find(
+      (hour: any) => hour.day_of_week === today,
+    )
+    if (!todayHours) return
 
-  if (!todayHours) {
-    return null // Se não houver horário cadastrado para hoje, não faz nada
-  }
+    const openTime = parse(todayHours.open_time, 'HH:mm:ss', now)
+    const closeTime = parse(todayHours.close_time, 'HH:mm:ss', now)
+    const shouldBeOpen = isAfter(now, openTime) && isBefore(now, closeTime)
 
-  const openTime = parse(todayHours.open_time, 'HH:mm:ss', now)
-  const closeTime = parse(todayHours.close_time, 'HH:mm:ss', now)
-
-  // Verifica se a loja deveria estar aberta ou fechada
-  const shouldBeOpen = isAfter(now, openTime) && isBefore(now, closeTime)
-
-  // Se o status atual estiver incorreto, atualiza no Supabase
-  if (shouldBeOpen !== store?.is_open) {
-    useMutation({
-      mutationKey: ['store-status'],
-      mutationFn: () => storeShouldBeOpen(shouldBeOpen, store.id),
-      onError: () => console.error('Erro ao atualizar status da loja'),
-    })
-  }
+    if (shouldBeOpen !== store?.is_open) {
+      updateStoreStatus(shouldBeOpen)
+    }
+  }, [store, updateStoreStatus])
 
   return null
 }
