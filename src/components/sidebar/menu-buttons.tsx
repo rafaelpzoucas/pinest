d3b55e25cb555f4eab3b7dd2c86bff82af83b434
Rafael Zoucas <rafaelpzoucas@gmail.com>
2026-01-07 10:03:18 -0300

    Update menu-buttons.tsx

--- src/components/sidebar/menu-buttons.tsx
+++ src/components/sidebar/menu-buttons.tsx
@@ -1,38 +1,38 @@
-'use client'
+"use client";
 
-import { ChevronLeft } from 'lucide-react'
-import Link from 'next/link'
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 import {
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
+  SidebarMenuItem,
   SidebarTrigger,
   useSidebar,
-} from '../ui/sidebar'
-import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip'
-import { main, reports, settings } from './items'
+} from "../ui/sidebar";
+import { main, reports, settings } from "./items";
 
 export function SidebarButtons() {
-  const { state } = useSidebar()
+  const { state } = useSidebar();
 
-  const isCollapsed = state === 'collapsed'
+  const isCollapsed = state === "collapsed";
 
   const groups = [
     {
-      label: 'Principal',
+      label: "Principal",
       items: main,
     },
     {
-      label: 'Configurações',
+      label: "Configurações",
       items: settings,
     },
     {
-      label: 'Relatórios',
+      label: "Relatórios",
       items: reports,
     },
-  ]
+  ];
 
   return (
     <>
@@ -48,32 +48,20 @@ export function SidebarButtons() {
           <SidebarGroupLabel>{group.label}</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {group.items.map((item) =>
-                isCollapsed ? (
-                  <Tooltip delayDuration={0} key={item.url}>
-                    <TooltipTrigger asChild>
-                      <SidebarMenuButton>
-                        <Link href={item.url}>
-                          <item.icon className="w-4 h-4" />
-                          <span className="sr-only">{item.title}</span>
-                        </Link>
-                      </SidebarMenuButton>
-                    </TooltipTrigger>
-                    <TooltipContent side="right">{item.title}</TooltipContent>
-                  </Tooltip>
-                ) : (
-                  <SidebarMenuButton asChild key={item.url}>
+              {group.items.map((item) => (
+                <SidebarMenuItem key={item.url}>
+                  <SidebarMenuButton asChild tooltip={item.title}>
                     <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
                     </Link>
                   </SidebarMenuButton>
-                ),
-              )}
+                </SidebarMenuItem>
+              ))}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
       ))}
     </>
-  )
+  );
 }
