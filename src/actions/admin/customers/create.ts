5bb380dc55e8212e83c6f7ab51d9c970d22f01bd
Rafael Zoucas <rafaelpzoucas@gmail.com>
2026-01-16 11:07:53 -0300

    fix: make phone optional

--- src/actions/admin/customers/create.ts
+++ src/actions/admin/customers/create.ts
@@ -1,46 +1,46 @@
-import { createClient } from '@/lib/supabase/server'
-import { StoreCustomerType } from '@/models/store-customer'
-import { createServerAction, ZSAError } from 'zsa'
-import { createAdminCustomerSchema } from './schemas'
+import { createClient } from "@/lib/supabase/server";
+import { StoreCustomerType } from "@/models/store-customer";
+import { createServerAction, ZSAError } from "zsa";
+import { createAdminCustomerSchema } from "./schemas";
 
 export const createAdminCustomerServer = createServerAction()
   .input(createAdminCustomerSchema)
   .handler(async ({ input }) => {
-    const supabase = createClient()
+    const supabase = createClient();
 
-    const { phone, name, address, storeId } = input
+    const { phone, name, address, storeId } = input;
 
     const { data: createdCustomer, error: createCustomerError } = await supabase
-      .from('customers')
-      .insert({ phone, name, address })
+      .from("customers")
+      .insert({ phone: phone || null, name, address })
       .select()
-      .single()
+      .single();
 
     if (createCustomerError) {
       throw new ZSAError(
-        'INTERNAL_SERVER_ERROR',
-        createCustomerError.message ?? 'Error creating customers.',
-      )
+        "INTERNAL_SERVER_ERROR",
+        createCustomerError.message ?? "Error creating customers.",
+      );
     }
 
     const { data: createdStoreCustomer, error: createStoreCustomerError } =
       await supabase
-        .from('store_customers')
+        .from("store_customers")
         .insert({ customer_id: createdCustomer?.id, store_id: storeId })
         .select()
-        .single()
+        .single();
 
     if (createStoreCustomerError) {
       throw new ZSAError(
-        'INTERNAL_SERVER_ERROR',
-        createStoreCustomerError.message ?? 'Error adding customer to store.',
-      )
+        "INTERNAL_SERVER_ERROR",
+        createStoreCustomerError.message ?? "Error adding customer to store.",
+      );
     }
 
     const createdStoreCustomerData: StoreCustomerType = {
       ...createdStoreCustomer,
       customers: createdCustomer,
-    }
+    };
 
-    return { createdStoreCustomer: createdStoreCustomerData }
-  })
+    return { createdStoreCustomer: createdStoreCustomerData };
+  });
